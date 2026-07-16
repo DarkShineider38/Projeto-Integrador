a1f44b551ca6af67743ee8db398ee71c928471a2
djeehdev <sdjevelli@gmail.com>
2026-07-15 21:35:01 -0300

    att v0.6

--- PHP/ReservasSalasLab.php
+++ PHP/ReservasSalasLab.php
@@ -14,7 +14,7 @@
         }
 
         function cadastrarReserva(){
-            $sql = "INSERT INTO reservasLab (id_sala, inicio_data, fim_data, horario_inico, horario_final, id_usuario) VALUES($1,$2,$3,$4,$5,$6)";
+            $sql = "INSERT INTO reservasLab (id_salas_lab, inicio_data, fim_data, horario_inico, horario_final, id_usuario) VALUES($1,$2,$3,$4,$5,$6)";
             $result = pg_query_params($this->dbconn, $sql, array($this->id_sala, $this->inicio_data, $this->fim_data, $this->horario_inicio, $this->horario_final, $this->id_usuario));
 
             if($result){
@@ -28,12 +28,12 @@
         }
 
         function editarReserva(){
-            $sql = "select id_reserva_equip from reserva_equip where id_reserva_equip = $1";
-            $result = pg_query_params($this->dbconn, $sql, array($this->id_reserva_equip));
+            $sql = "select id_salas_lab from reservasLab where id_salas_lab = $1";
+            $result = pg_query_params($this->dbconn, $sql, array($this->id_reserva_sala));
 
             $registro = pg_fetch_assoc($result);
             if($registro){
-                $query = "update reserva_equip set id_equip = $1, inicio_data = $2, fim_data = $3, horario_inico = $4, horario_final = $5, id_usuario = $6 where id_reserva_equip = ".$registro['id_reserva_equip'];
+                $query = "update reservasLab set id_salas_lab = $1, inicio_data = $2, fim_data = $3, horario_inico = $4, horario_final = $5, id_usuario = $6 where id_salas_lab = ".$registro['id_salas_lab'];
                 $stmt = pg_prepare($this->dbconn, "update_query", $query);
                 $result2 = pg_execute($this->dbconn, "update_query", array($this->id_equip, $this->inicio_data, $this->fim_data, $this->horario_inicio, $this->horario_final, $this->id_usuario));
                 if ($stmt) {
